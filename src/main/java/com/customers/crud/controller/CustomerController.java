0a4ed66dbb18597096bebd3f58f63e9d2d22e505
NL <nl@NLs-MacBook-Pro.local>
2020-01-07 15:18:04 +1100

    RUD operations

--- src/main/java/com/customers/crud/controller/CustomerController.java
+++ src/main/java/com/customers/crud/controller/CustomerController.java
@@ -4,6 +4,7 @@ import com.customers.crud.model.Customer;
 import com.customers.crud.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,6 +30,37 @@ public class CustomerController {
     @GetMapping("/customers/{id}")
     public Optional<Customer> getCustomerById(@PathVariable(value = "id") Long customerId) throws ChangeSetPersister.NotFoundException {
         return customerRepository.findById(customerId);
+    }
+
+    @GetMapping("/customers/{id}")
+    public Customer getNoteById(@PathVariable(value = "id") Long customerId) {
+        return customerRepository.findById(customerId)
+                .orElseThrow(() -> new IllegalArgumentException(Long.toString(customerId)));
+    }
+
+    @PutMapping("/customers/{id}")
+    public Customer updateNote(@PathVariable(value = "id") Long customerId,
+                           @Valid @RequestBody Customer customerDetails) {
+
+        Customer customer = customerRepository.findById(customerId)
+                .orElseThrow(() -> new IllegalArgumentException(Long.toString(customerId)));
+
+        customer.setName(customerDetails.getName());
+        customer.setAddress(customerDetails.getAddress());
+        customer.setPhone(customerDetails.getPhone());
+
+        Customer updatedCustomer = customerRepository.save(customer);
+        return updatedCustomer;
+    }
+
+    @DeleteMapping("/customers/{id}")
+    public ResponseEntity<?> deleteNote(@PathVariable(value = "id") Long customerId) {
+        Customer customer = customerRepository.findById(customerId)
+                .orElseThrow(() -> new IllegalArgumentException(Long.toString(customerId)));
+
+        customerRepository.delete(customer);
+
+        return ResponseEntity.ok().build();
     }
 
 
