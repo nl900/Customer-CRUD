ae77d0a49ba0adaed048fde424fc42c12d99131f
Nancy <nancyl1@student.unimelb.edu.au>
2020-01-12 08:49:11 +1100

    enable add/edit functionality

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home.js';
 import CustomerList from './components/CustomerList';
+import CustomerEdit from './components/CustomerEdit.js';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
       <Switch>
         <Route path='/' exact={true} component={Home} />
         <Route path='/customers' exact={true} component={CustomerList} />
+        <Route path='/customers/:id' component={CustomerEdit}/>
       </Switch>
       </Router >
     )
